cb9b52611d2d25fd7b6dc08315ed1d7285ba7de4
nurbol <daniarbeknurbol@gmail.com>
2021-10-27 09:57:08 +0300

    ch

--- main3.cpp
+++ main3.cpp
@@ -1,2 +1,14 @@
-ghfffghfhjffg
-ghfffghfhjffg2
+#include <iostream>
+using namespace std;
+int main()
+{
+    int result = 0;
+    for(int i = 5; i < 25; i++)
+    {
+        if (i % 2 == 0)
+        {
+            result += i;
+        }
+    }
+    cin << result;
+}
